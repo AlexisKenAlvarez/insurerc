5d29541d5cf4d34e9963528acc732371e12c552c
KoichiDevs <alexisken1432@gmail.com>
2023-03-18 21:02:24 +0800

    fixed roadmap color

--- src/components/RoadmapCard.jsx
+++ src/components/RoadmapCard.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 
 const RoadmapCard = ({ list, phase }) => {
     return (
-        <motion.div initial={{scale: 0.5, opacity: 0}} whileInView={{scale: 1, opacity: 100}} transition={{duration: 0.9, type: 'spring'}} className="h-[33rem] sm:w-[24rem] w-[18rem] bg-gradient-to-r from-hero to-[#231b35] rounded-2xl shadow-xl p-8 mt-20 border-l-8 border-l-mybrown border-r-8 border-r-mylight">
+        <motion.div initial={{scale: 0.5, opacity: 0}} whileInView={{scale: 1, opacity: 100}} transition={{duration: 0.9, type: 'spring'}} className="h-[33rem] sm:w-[24rem] w-[18rem] bg-gradient-to-r from-hero to-[#1b1409] rounded-2xl shadow-xl p-8 mt-20 border-l-8 border-l-mybrown border-r-8 border-r-mylight">
             <div className="relative w-fit">
                 <h1 className="font-saira text-xl font-semibold">{phase}</h1>
                 <div className="w-full h-[3px] bg-fade mt-2"></div>
@@ -16,7 +16,7 @@ const RoadmapCard = ({ list, phase }) => {
                 {list.map((items, i) => {
                     return (
                         <div className="flex items-center gap-x-3 justify-items-start" key={i}>
-                            <div className="h-9 w-11 bg-[#231b35] rounded-full flex items-center justify-center">
+                            <div className="h-9 w-11 bg-[#442f03] rounded-full flex items-center justify-center">
                                 <BsCheck className="text-2xl text-fade" />
                             </div>
 
